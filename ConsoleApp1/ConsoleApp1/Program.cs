ea75128049b7412fb5b7fba57f82af26f7d7a2ac
fu xu <xufu2022@gmail.com>
2022-05-10 04:50:05 +0100

    update-2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -20,25 +20,33 @@ internal class Program
 {
     public delegate void WriteMessageFunction(string message);
 
-    public static void WriteMessage(string message)
+    public static bool WriteMessage(string message)
     {
         Console.WriteLine(message);
+        return true;
     }    
-    public static void WriteTimelyMessage(string message)
+    public static bool WriteTimelyMessage(string message)
     {
         Console.WriteLine(message + " " + DateTime.Now);
+        return true;
     }
 
     static void Main(string[] args)
     {
         var selection = Console.ReadLine();
-        WriteMessageFunction func ;
+        //Action<string> func ;
+        Func<string,bool> func ;
         if(selection =="1")
          func = WriteTimelyMessage;
         else
         {
             func = WriteMessage;
         }
-        func("Hello World");
+        ExecuteWrite(func);
+    }
+
+    private static bool ExecuteWrite(Func<string, bool> func)
+    {
+       return func("Hello World");
     }
 }
